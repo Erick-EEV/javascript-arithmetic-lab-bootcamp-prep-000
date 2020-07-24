4446518fddac97c8e01266197b60bc616d758466
Erick Villeda <eeverickeev@gmail.com>
2020-07-24 18:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function add(){
+function add(a, b){
   return (1 + 80)
 }
-function subtract(){
+function subtract(a, b){
   return (60 - 40)
 }
-function multiply(){
+function multiply(a, b){
   return (2 * 3.4)
 }
-function divide(){
+function divide(a, b){
 return (5.0 / 2.5)
 }
 var number = 10
