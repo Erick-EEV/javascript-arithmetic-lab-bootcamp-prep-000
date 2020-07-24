33900defff7e4a3595547e7a1d288bf56be497b8
Erick Villeda <eeverickeev@gmail.com>
2020-07-24 17:39:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(1 + 80){
+  return `result is ${1 + 80}.`
+}
+function subtract(60 - 40){
+  return `result is ${60 - 40}`
+}
+function multiply(1 * 3.4){
+  return `result is ${1 * 3.4}`
+}
+function divide(5.0 / 2.5){
+return `result is ${5.0 / 2.5}`
+}
