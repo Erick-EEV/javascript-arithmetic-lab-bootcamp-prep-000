264997e6a89b77f4b162dd85451331ee905ac131
Erick Villeda <eeverickeev@gmail.com>
2020-07-24 17:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-function add(a, b){
+function add(){
   return (1 + 80)
 }
-function subtract(a, b){
+function subtract(){
   return (60 - 40)
 }
-function multiply(a, b){
+function multiply(){
   return (2 * 3.4)
 }
-function divide(a, b){
+function divide(){
 return (5.0 / 2.5)
 }
+var number = 10
+
+function add5(){
+  number += 5
+}
+function divideBy3(){
+  number/= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
